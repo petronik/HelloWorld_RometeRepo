06c39348a598a1eafa04c385496e2fa209cb84fc
Mike Zorin <mike.zorin@robertsoncollege.ca>
2021-06-25 10:28:22 -0500

    adde feture 1

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,6 +7,12 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+            // new feature
+            int res = 5 * 4;
+
+            Console.WriteLine("res = " + res);
         }
     }
 }
