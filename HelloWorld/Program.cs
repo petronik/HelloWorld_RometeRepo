923df57448181b5631d0c07d8325a85abc244ea3
Mike Zorin <mike.zorin@robertsoncollege.ca>
2021-06-28 09:43:28 -0500

    added Class Activity (compleate it)

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Globalization;
+using System.Runtime.InteropServices;
 
 namespace HelloWorld
 {
@@ -6,16 +9,45 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+            Console.WriteLine("Enter your first name:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter your last name:");
+            string lastName = Console.ReadLine();
+
+            // Console.WriteLine("Hello " + firstName + "!");
+            Console.WriteLine("Hello {1} {0} {1}!", firstName, lastName);
+            Console.Write($"Hello {firstName} {lastName}");
+
+            Console.WriteLine("!");
+            */
+
+            /*
+            var date = DateTime.Now;
+            Console.WriteLine($"Today is {date.DayOfWeek}, {date:dd-MM-yyyy}");
+
+            string name = "Mark";
+            int age = 32; 
+            Console.WriteLine($"{name} is {age} year{(age == 1 ? "" : "s")} old.");
+
+            decimal salary = 1500.55m;
+            Console.WriteLine($"{name}'s salary is {salary.ToString("C2", CultureInfo.CreateSpecificCulture("ja-JP"))}");
 
 
-            // new feature
-            int res = 5 * 4;
+            int i = 123456;
+            Console.WriteLine($"i = {i:D}");
+            Console.WriteLine($"i = {i:N2}");
 
-            Console.WriteLine("res = " + res);
+            float f = 0.5f;
+            Console.WriteLine($"p = {f:P}");
 
-            // feature 2
-            Console.WriteLine("Second feature here!");
+            int c = 255;
+            Console.WriteLine($"c = {c:X}");
+            */
+            const int w = 7;
+            Console.WriteLine($"|{"sbyte",-w}|{sizeof(sbyte),w}|{sbyte.MinValue,w}|{sbyte.MaxValue,w}|");
+            Console.WriteLine($"|{"int",-w}|{sizeof(int),w}|{int.MinValue,w}|{int.MaxValue,w}|");
         }
     }
 }
---
+++ HelloWorld/Task_01.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    public class Task_01
+    {
+        /*
+        Task
+        Your task is to find the similarity of given sorted arrays a and b, 
+        which is defined as follows:
+
+        you take the number of elements which are present in both arrays 
+        and divide it by the number of elements which are present in at least one array.
+
+        It also can be written as a formula similarity(A, B) = #(A ∩ B) / #(A ∪ B), 
+        where #(C) is the number of elements in C, ∩ is intersection of arrays, 
+        ∪ is union of arrays.
+
+        This is known as Jaccard similarity.
+
+        The result is guaranteed to fit any floating-point type without rounding.
+
+        Example
+        For a = [1, 2, 4, 6, 7] and b = [2, 3, 4, 7]:
+
+        elements [2, 4, 7] are present in both arrays;
+        elements [1, 2, 3, 4, 6, 7] are present in at least one of the arrays.
+        So the similarity equals to 3 / 6 = 0.5.
+
+        Tests:
+        Similarity(new int[]{1, 2, 3},new int[]{1, 2, 3}); // 1
+        Similarity(new int[]{1, 2, 3},new int[]{4, 5, 6}); // 0
+        Similarity(new int[]{1, 2, 4, 6, 7},new int[]{2, 3, 4, 7}); // 0.5
+        Similarity(new int[]{1, 2, 6, 8, 9},new int[]{0, 1, 4, 5, 6, 8, 9}); // 0.5
+        Similarity(new int[]{1, 2, 3, 4, 7, 9},new int[]{1, 2, 3}); // 0.5
+        Similarity(new int[]{0, 1, 3, 4, 5, 6, 9, 14, 15, 16, 17, 18, 19},new int[]{1, 4, 10, 12, 13, 14, 15, 16}) // 0.3125;
+         */
+        public static double Similarity(int[] a, int[] b)
+        {
+            //coding and coding..
+
+            return 0;
+        }
+    }
+}
